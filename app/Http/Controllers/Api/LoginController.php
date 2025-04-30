678fe79d356c5d436feecd6e518ad454d0b3ba23
GeovanniMercz <geza.mercz@cceo.com.mx>
2025-04-30 11:21:52 -0600

    fix 🐛 Change the message that return to success

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -38,7 +38,7 @@ class LoginController extends Controller
                 $request->password === $user['password']
             ) {
                 return response()->json([
-                    'message' => 'Login exitoso',
+                    'message' => 'success',
                     'user' => $user
                 ]);
             }
