71c0e5e95a7442b7b896c82918733aa55d2eafc8
GeovanniMercz <geza.mercz@cceo.com.mx>
2025-05-19 19:49:06 -0600

    feat :sparkles: Add more groups to send at application

--- app/Http/Controllers/Api/GrupoPreparacionController.php
+++ app/Http/Controllers/Api/GrupoPreparacionController.php
@@ -9,24 +9,35 @@ class GrupoPreparacionController extends Controller
 {
     public function index()
     {
-        // Datos de grupos en preparación simulados (arreglo)
-        $grupos_en_preparacion = [
-            [
-                'id' => 1,
-                'materia' => 'Matemáticas',
-                'numero_creditos' => 4,
-                'maestro' => 'Dr. López',
-                'horario' => 'Lunes 8:00 - 10:00',
-            ],
-            [
-                'id' => 2,
-                'materia' => 'Física',
-                'numero_creditos' => 3,
-                'maestro' => 'Ing. Pérez',
-                'horario' => 'Miércoles 10:00 - 12:00',
-            ],
+        $grupos_en_preparacion = [];
+
+        $materias = [
+            'Matemáticas', 'Física', 'Química', 'Programación', 'Historia', 'Literatura', 'Biología',
+            'Álgebra', 'Geometría', 'Inglés', 'Contabilidad', 'Administración', 'Ética',
+            'Economía', 'Psicología', 'Estadística', 'Cálculo', 'Inteligencia Artificial',
+            'Bases de Datos', 'Redes', 'Sistemas Operativos', 'Desarrollo Web', 'Seguridad Informática',
+            'Ingeniería de Software', 'Análisis de Algoritmos'
         ];
+
+        $maestros = ['Dr. López', 'Ing. Pérez', 'Mtra. Sánchez', 'Lic. Gómez', 'Dra. Ortega', 'Prof. Ramírez'];
+
+        $horarios = [
+            'Lunes 8:00 - 10:00', 'Martes 10:00 - 12:00', 'Miércoles 12:00 - 14:00',
+            'Jueves 14:00 - 16:00', 'Viernes 16:00 - 18:00', 'Sábado 9:00 - 11:00'
+        ];
+
+        for ($i = 1; $i <= 100; $i++) {
+            $grupos_en_preparacion[] = [
+                'id' => $i,
+                'materia' => $materias[array_rand($materias)],
+                'numero_creditos' => rand(2, 5),
+                'semestre' => (string)rand(1, 10),
+                'maestro' => $maestros[array_rand($maestros)],
+                'horario' => $horarios[array_rand($horarios)],
+            ];
+        }
 
         return response()->json($grupos_en_preparacion);
     }
 }
+
